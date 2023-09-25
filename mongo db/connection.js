e0469dfb21afc95ec46ca46ee25e68e1c79baf01
AbleKSaju <ableksaju3@gmail.com>
2023-09-25 14:00:33 +0530

    compass connected

--- mongo db/connection.js
+++ mongo db/connection.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose')
 
 module.exports={
     connect(){
-        mongoose.connect('mongodb://0.0.0.0:27017/sampl',{
+        mongoose.connect(process.env.mongoDB_URL,{
             useNewUrlParser:true,useUnifiedTopology:true
         }).then((data)=>{
             console.log("Database Connected");
